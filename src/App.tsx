d4054c5f38ba41da1521ff660a6d750b963f21f0
otuekong <myshield02@gmail.com>
2024-11-05 09:27:23 +0100

    added path to dashboard, create organization template, login

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 import Welcome from './pages/Welcome';
+import Dashboard from "./pages/Dashboard.tsx";
+import {CreateOrganization} from "./pages/organization/Create.tsx";
+import {Login} from "./pages/auth/Login.tsx";
 
 const App = () => {
 
   return (
     <>
       <Routes>
-        {/*<Route path='/create' element={<CreateOrganization />} />*/}
+        <Route path='/create' element={<CreateOrganization />} />
         <Route path='/onboarding/welcome' element={<Welcome />} />
         {/*<Route path='/accounts/register' element={<Register />} />*/}
 
-        {/*<Route path='/auth/login' element={<Login />} />*/}
-        {/*<Route path='/' element={<Dashboard />} />*/}
+        <Route path='/auth/login' element={<Login />} />
+        <Route path='/' element={<Dashboard />} />
       </Routes>
     </>
   );
