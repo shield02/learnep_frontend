8a88cec809764e493a065419d05feb6774539aca
otuekong <myshield02@gmail.com>
2024-11-05 09:28:10 +0100

    added and commented out configuration for scss

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@import "src/_metronic/assets/sass/style.scss";`,
+  //     },
+  //   },
+  // },
   server: {
     host: '0.0.0.0',
     port: 5173,
